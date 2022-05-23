613d934a8c6820391d09e786b0dc8236ddf26f56
Anna Melashkina <anna.osiatnik@gmail.com>
2022-05-23 16:24:48 +0300

    added user roles

---
+++ src/Imgeneus.World/Pages/Users/UserDTO.cs
@@ -0,0 +1,31 @@
+﻿using Imgeneus.Database.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Imgeneus.World.Pages.Users
+{
+    public class UserDTO
+    {
+        public string UserName { get; }
+
+        public uint Points { get; }
+
+        public string Faction { get; }
+
+        public IEnumerable<string> Roles { get; }
+
+        public DateTime LastConnectionTime { get; }
+
+        public bool IsDeleted { get; }
+
+        public UserDTO(DbUser user, IEnumerable<string> roles)
+        {
+            UserName = user.UserName;
+            Points = user.Points;
+            Faction = user.Faction.ToString();
+            LastConnectionTime = user.LastConnectionTime;
+            IsDeleted = user.IsDeleted;
+            Roles = roles;
+        }
+    }
+}
