844792615089d25ee20101b13cf1e4168552049b
Anna Melashkina <anna.osiatnik@gmail.com>
2022-09-18 17:07:20 +0300

    invite to party

--- src/Imgeneus.World/Handlers/PartySearchInviteHandler.cs
+++ src/Imgeneus.World/Handlers/PartySearchInviteHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Imgeneus.Network.Packets;
 using Imgeneus.Network.Packets.Game;
+using Imgeneus.World.Game.Country;
 using Imgeneus.World.Game.PartyAndRaid;
 using Imgeneus.World.Game.Session;
 using Imgeneus.World.Game.Zone;
@@ -13,16 +14,22 @@ namespace Imgeneus.World.Handlers
     public class PartySearchInviteHandler : BaseHandler
     {
         private readonly IMapProvider _mapProvider;
+        private readonly ICountryProvider _countryProvider;
 
-        public PartySearchInviteHandler(IGamePacketFactory packetFactory, IGameSession gameSession, IMapProvider mapProvider) : base(packetFactory, gameSession)
+        public PartySearchInviteHandler(IGamePacketFactory packetFactory, IGameSession gameSession, IMapProvider mapProvider, ICountryProvider countryProvider) : base(packetFactory, gameSession)
         {
             _mapProvider = mapProvider;
+            _countryProvider = countryProvider;
         }
 
         [HandlerAction(PacketType.PARTY_SEARCH_INVITE)]
         public void Handle(WorldClient client, PartySearchInvitePacket packet)
         {
-            var requestedPlayer = _mapProvider.Map.PartySearchers.FirstOrDefault(p => p.AdditionalInfoManager.Name == packet.Name);
+            var requestedPlayer = _mapProvider.Map.PartySearchers.FirstOrDefault(p => p.CountryProvider.Country == _countryProvider.Country && p.AdditionalInfoManager.Name == packet.Name);
+
+            if (requestedPlayer is null)
+                requestedPlayer = _mapProvider.Map.Players.Values.FirstOrDefault(p => p.CountryProvider.Country == _countryProvider.Country && p.AdditionalInfoManager.Name == packet.Name);
+
             if (requestedPlayer != null && !requestedPlayer.PartyManager.HasParty)
             {
                 requestedPlayer.PartyManager.InviterId = _gameSession.Character.Id;
