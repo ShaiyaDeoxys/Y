d175286259668fb6f59b78da2e7ae786e13372fa
Anna Melashkina <anna.osiatnik@gmail.com>
2022-09-22 09:27:46 +0300

    event off command

--- src/Imgeneus.World/Handlers/GMCurePlayerHandler.cs
+++ src/Imgeneus.World/Handlers/GMCurePlayerHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Imgeneus.Network.Packets;
 using Imgeneus.Network.Packets.Game;
 using Imgeneus.World.Game;
+using Imgeneus.World.Game.Buffs;
 using Imgeneus.World.Game.Session;
 using Imgeneus.World.Packets;
 using Sylver.HandlerInvoker.Attributes;
@@ -12,10 +13,12 @@ namespace Imgeneus.World.Handlers
     public class GMCurePlayerHandler : BaseHandler
     {
         private readonly IGameWorld _gameWorld;
+        private readonly IBuffsManager _buffsManager;
 
-        public GMCurePlayerHandler(IGamePacketFactory packetFactory, IGameSession gameSession, IGameWorld gameWorld) : base(packetFactory, gameSession)
+        public GMCurePlayerHandler(IGamePacketFactory packetFactory, IGameSession gameSession, IGameWorld gameWorld, IBuffsManager buffsManager) : base(packetFactory, gameSession)
         {
             _gameWorld = gameWorld;
+            _buffsManager = buffsManager;
         }
 
         [HandlerAction(PacketType.GM_CURE_PLAYER)]
@@ -54,6 +57,32 @@ namespace Imgeneus.World.Handlers
 
             target.HealthManager.FullRecover();
             target.SkillsManager.Cooldowns.Clear();
+
+            _packetFactory.SendGmCommandSuccess(client);
+        }
+
+        [HandlerAction(PacketType.GM_CLEAR_BUFFS)]
+        public void HandleClearBuffs(WorldClient client, GMCurePlayerPacket packet)
+        {
+            if (!_gameSession.IsAdmin)
+                return;
+
+            if (string.IsNullOrEmpty(packet.Name))
+                return;
+
+            var target = _gameWorld.Players.FirstOrDefault(p => p.Value.AdditionalInfoManager.Name == packet.Name).Value;
+            if (target == null)
+            {
+                _packetFactory.SendGmCommandError(client, PacketType.GM_CLEAR_BUFFS);
+                return;
+            }
+
+            target.HealthManager.FullRecover();
+            target.SkillsManager.Cooldowns.Clear();
+            foreach(var buff in _buffsManager.ActiveBuffs.ToList())
+            {
+                buff.CancelBuff();
+            }
 
             _packetFactory.SendGmCommandSuccess(client);
         }
